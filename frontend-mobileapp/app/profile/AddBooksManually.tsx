4c748bd7e9472ba0b9869fcfc3f010813f5c16a1
itshoto <satavisa.borno@northsouth.edu>
2024-11-13 01:29:18 +0600

    Update AddBooksManually.tsx

--- frontend-mobileapp/app/profile/AddBooksManually.tsx
+++ frontend-mobileapp/app/profile/AddBooksManually.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import { Colors } from '@/constants/Colors';
 
 export default function AddBookManuallyScreen() {
   const [title, setTitle] = useState('');
@@ -11,14 +12,79 @@ export default function AddBookManuallyScreen() {
 
   return (
     <View style={styles.container}>
-      <TextInput placeholder="Book Title" value={title} onChangeText={setTitle} style={styles.input} />
-      <TextInput placeholder="Author" value={author} onChangeText={setAuthor} style={styles.input} />
-      <Button title="Save Book" onPress={handleSaveBook} />
+      <TouchableOpacity style={styles.profilePicContainer}>
+        <Image source={{ uri: 'https://example.com/Book.jpg' }} style={styles.profilePic} />
+        <Text style={styles.editPhotoText}>Edit Photo</Text>
+      </TouchableOpacity>
+      <TextInput
+        style={styles.input}
+        value={title}
+        onChangeText={setTitle}
+        placeholder="Book Name"
+        placeholderTextColor="#888"
+      />
+      <TextInput
+        style={styles.input}
+        value={author}
+        onChangeText={setAuthor}
+        placeholder="Author Name"
+        placeholderTextColor="#888"
+      />
+      <TouchableOpacity style={styles.saveButton} onPress={handleSaveBook}>
+        <Text style={styles.saveButtonText}>Save Book</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, padding: 16 },
-  input: { padding: 10, borderWidth: 1, borderColor: '#ccc', borderRadius: 5, marginBottom: 10 },
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.background,
+  },
+  profilePicContainer: {
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  profilePic: {
+    width: 150,
+    height: 200, 
+    borderRadius: 10,
+    marginBottom: 8,
+    backgroundColor: '#ddd',
+  },
+  editPhotoText: {
+    fontSize: 16,
+    color: Colors.choco
+  },
+  input: {
+    width: '100%',
+    height: 50,
+    padding: 10,
+    marginVertical: 10,
+    borderColor: Colors.choco,
+    borderWidth: 1,
+    borderRadius: 8,
+    backgroundColor: Colors.savoy,
+    fontSize: 16,
+  },
+  saveButton: {
+    marginTop: 20,
+    width: '100%',
+    height: 50,
+    backgroundColor: Colors.savoy,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8,
+  },
+  saveButtonText: {
+    fontSize: 18,
+    color: Colors.background,
+    fontWeight: 'bold',
+  },
 });
+
+
