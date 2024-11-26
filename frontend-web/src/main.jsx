c85fef34e2301268191436870a3e1bf1fe082bad
ramizarahman <ramizarb25@gmail.com>
2024-11-26 14:17:42 +0600

    Update main.jsx

--- frontend-web/src/main.jsx
+++ frontend-web/src/main.jsx
@@ -1,8 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
 
 // Creating the root of the React app
 const root = ReactDOM.createRoot(document.getElementById('root'));
