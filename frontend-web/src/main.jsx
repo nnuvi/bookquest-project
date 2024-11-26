24ef3c1a225265a3ebff4eb19ab19d0288c5fc16
ramizarahman <ramizarb25@gmail.com>
2024-11-26 14:14:23 +0600

    Update main.jsx

--- frontend-web/src/main.jsx
+++ frontend-web/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// Creating the root of the React app
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Rendering the application inside BrowserRouter to enable routing
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 )
